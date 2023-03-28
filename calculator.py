45db4377fd12f152c43276bf00aebc1e7a5b5117
Blessing Boaslah <blessing@kwagei.com>
2023-03-28 22:55:07 +0000

    starting again

--- calculator.py
+++
@@ -1,9 +0,0 @@
-print("Hello, this is Blessing's calculator.");
-num1 = int(input("Please enter the first number: "));
-operator = input("Enter an operator: ");
-num2 = int(input("Please enter the second number: "));
-if(operator == "*"):
-    result = num1 * num2;
-    print(result);
-
-    
